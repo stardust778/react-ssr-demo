52c3c39af3363151ee3abcaf0c76003331af3b7f
stardust778 <lirihao911@163.com>
2023-05-12 10:50:39 +0800

    feat: 命令concurrently

--- webpack.client.js
+++ webpack.client.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.base');
 
-// 服务端的webpack
+// 客户端的webpack
 module.exports = merge(baseConfig, {
   mode: 'development',
   entry: './src/client/index.tsx',
--- webpack.server.js
+++ webpack.server.js
@@ -5,11 +5,14 @@ const baseConfig = require('./webpack.base');
 
 // 服务端的webpack
 module.exports = merge(baseConfig, {
+  // 为了不把 Node.js 内置的模块打包进输出文件中，例如 fs net 模块等
   target: 'node',
   mode: 'development',
   entry: './src/server/index.tsx',
+  // 为了不把 node_modules 目录下的第三方模块打包进输出文件中
   externals: [nodeExternals()],
   output: {
+    libraryTarget: 'commonjs2',
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'server_build')
   },
